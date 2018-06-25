af42c0129043a459eaec172e492e3d14d789c14b
Jakubem <jakubmx@gmail.com>
2018-06-25 23:17:05 +0200

    :(

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-var http = require('http').Server(app);
+const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const fs = require('fs');
 
