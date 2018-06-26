1b93683834ced321473a5ce62398b3cca74eea22
unknown <jakubmx@gmail.com>
2018-06-26 10:26:58 +0200

    add first working marker removal functionality

--- app.js
+++ app.js
@@ -11,29 +11,29 @@ app.use(express.static('public'))
 
 // serve index.html as root
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 // respond to client connection
 io.on('connection', (socket) => {
-    console.log('client is ready');
+  console.log('client is ready');
 
-    socket.emit('connectionEstablished', 'backend connected');
+  socket.emit('connectionEstablished', 'backend connected');
 
-    // get all pins from client...
-    socket.on('pins', (data)=>{
-        console.log(data);
+  // get all pins from client...
+  socket.on('pins', (data) => {
+    console.log(data);
 
-        // ...and save them to JSON file
-        writeToFile(data)
-    })
+    // ...and save them to JSON file
+    writeToFile(data)
+  })
 });
 
-function writeToFile(data){
-    fs.writeFileSync('all-pins.json', data);
+function writeToFile(data) {
+  fs.writeFileSync('all-pins.json', data);
 }
 
 
 http.listen(port, () => {
-    console.log(`listening on ${port}`);
+  console.log(`listening on ${port}`);
 });
--- public/index.js
+++ public/index.js
@@ -8,72 +8,82 @@ const removeMarkerBtn = document.querySelector('.remove-marker-btn');
 // create socket connection
 const socket = io('http://localhost:5500');
 
-socket.on('connectionEstablished', (data)=>{
-    console.log(data);
+socket.on('connectionEstablished', (data) => {
+  console.log(data);
 })
 
 function initMap() {
-    let myLatLng = {
-            lat: 53.36552,
-            lng: 14.64999
-        },
+  let myLatLng = {
+      lat: 53.36552,
+      lng: 14.64999
+    },
 
-        //initialize new google map instance
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: myLatLng,
-            zoom: 19,
-            mapTypeId: 'satellite',
-            draggableCursor: 'crosshair',
-        });
-
-    // add marker on click
-    map.addListener('click', (e) => {
-        placeMarker(e.latLng);
+    //initialize new google map instance
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: myLatLng,
+      zoom: 19,
+      mapTypeId: 'satellite',
+      draggableCursor: 'crosshair',
     });
 
-    //create empty array for all created markers
-    let markers = [];
+  // add marker on click
+  map.addListener('click', (e) => {
+    placeMarker(e.latLng);
+  });
 
-    // create marker based on click position
-    function placeMarker(pos) {
-        let marker = new google.maps.Marker({
-                position: pos,
-                map: map,
-                draggable: true,
-                // icon: './assets/img/pin.png',
-            },
-        );
+  //create empty array for all created markers
+  let markers = [];
 
-        // push each created marker to markers array
-        markers.push(marker)
-    }
+  // create marker based on click position
+  function placeMarker(pos) {
+    let marker = new google.maps.Marker({
+      position: pos,
+      map: map,
+      draggable: true,
+      // icon: './assets/img/pin.png',
+    }, );
 
-    // remove single marker
-    removeMarkerBtn.addEventListener('click', ()=>{
-        markers[2].setMap(null);
+    // push each created marker to markers array
+    markers.push(marker)
+  }
+
+  // remove single marker
+  removeMarkerBtn.addEventListener('click', () => {
+
+    // pass length of markers array reduced by 1 to removeMarker function
+    removeMarker(markers.length - 1);
+  })
+
+  function removeMarker(i) {
+
+    // remove single google maps marker
+    markers[i].setMap(null);
+
+    // remove ^ marker object from markers array
+    markers.splice(i, 1);
+  }
+
+  getPositionBtn.addEventListener('click', getMyLatLng);
+
+  // get position of each marker as JSON
+  function getMyLatLng() {
+
+    // create empty markersPosition array or reset existing one
+    markersPosition = [];
+
+    // loop through all created markers...
+    markers.forEach((el) => {
+      // ...and get each marker position
+      let lat = el.getPosition().lat();
+      let lng = el.getPosition().lng();
+      markersPosition.push(JSON.parse(`{"lat": ${lat},"lng": ${lng}}`));
     })
 
-    getPositionBtn.addEventListener('click', getMyLatLng);
-
-    // get position of each marker as JSON
-    function getMyLatLng() {
-
-        // create empty markersPosition array or reset existing one
-        markersPosition = [];
-
-        // loop through all created markers...
-        markers.forEach((el) => {
-            // ...and get each marker position
-            let lat = el.getPosition().lat();
-            let lng = el.getPosition().lng(); 
-            markersPosition.push(JSON.parse(`{"lat": ${lat},"lng": ${lng}}`));
-        })
-
-        const allPins = {
-            "pins": markersPosition,
-        }
-        // socket.emit('pins', allPins)
-        socket.emit('pins', JSON.stringify(allPins, null, 2))
-        console.log('send to backend: ' + JSON.stringify(allPins));
+    const allPins = {
+      "pins": markersPosition,
     }
+    // socket.emit('pins', allPins)
+    socket.emit('pins', JSON.stringify(allPins, null, 2))
+    console.log('send to backend: ' + JSON.stringify(allPins));
+  }
 }
